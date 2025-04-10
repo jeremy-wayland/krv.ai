9ca75b844dd9965a2484c6585daa4bb6a49944b4
Sidney Gathrid <sgathrid@yahoo.com>
2025-04-10 12:09:49 -0700

    add Open Graph Preview for generating previews from a link

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,9 +1,19 @@
 export default function Head() {
   return (
     <>
-      <title> Krv Analytics </title>
+      <title>Krv Analytics</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
+
+      {/* Open Graph Meta Tags */}
+      <meta property="og:title" content="Krv Analytics" />
+      <meta
+        property="og:description"
+        content="Transforming your data into intelligent, AI-powered insights"
+      />
+      <meta property="og:image" content="/images/logo/logo-dark.svg" />
+      <meta property="og:url" content="/app/favicon.ico" />
+
       <link rel="icon" href="/app/favicon.ico" />
     </>
   );
