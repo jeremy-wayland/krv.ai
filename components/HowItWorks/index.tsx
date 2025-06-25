c008b67bdd9394432eb9c11d001b4686952ca61b
Stuart Wayland <swayland@ucsc.edu>
2025-06-24 19:29:20 -0700

    rennaming about component on landing page

--- components/HowItWorks/index.tsx
+++ components/HowItWorks/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image";
-// import Mover from "@/images/about/moveTest";
 import Mover from "./nodeBoundce";
 
-const About = () => {
+const HowItWorks = () => {
   return (
     <>
       {/* <!-- ===== About Start ===== --> */}
@@ -38,13 +36,6 @@ const About = () => {
               <div className="hidden h-full w-full dark:block">
                 <Mover />
               </div>
-
-              {/* <Image
-                src="/images/about/about-dark-01.svg"
-                alt="About"
-                className="hidden dark:block"
-                fill
-              /> */}
             </motion.div>
             <motion.div
               variants={{
@@ -131,108 +122,8 @@ const About = () => {
           </div>
         </div>
       </section>
-      {/* <!-- ===== About End ===== --> */}
-
-      {/* <!-- ===== About Two Start ===== --> */}
-      {/* <section>
-        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
-          <div className="flex items-center gap-8 lg:gap-32.5">
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: -20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_left md:w-1/2"
-            >
-              <h4 className="font-medium uppercase text-black dark:text-white">
-                Stear Clear Of
-              </h4>
-              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                Data {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
-                  Overload
-                </span>
-              </h2>
-              <p>
-                Traditional tools drown you in information without making sense
-                of it. Our tools doesn’t just surface patterns—they understand
-                relationships, trends, and context, turning raw data into clear,
-                actionable insights.
-              </p>
-              <br />
-              <p>
-                Gone are the days of trying to solve a massive puzzle with
-                thousands of scattered pieces. Instead of guessing where they
-                fit, we assemble the big picture for you—so you see the whole
-                story instantly. overload.
-              </p>
-              <div>
-                <a
-                  href="/support"
-                  className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary"
-                >
-                  <span className="duration-300 group-hover:pr-2">
-                    Know More
-                  </span>
-                  <svg
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="currentColor"
-                  >
-                    <path d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z" />
-                  </svg>
-                </a>
-              </div>
-            </motion.div>
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: 20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_right relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2"
-            >
-              <Image
-                src="./images/about/about-light-02.svg"
-                alt="About"
-                className="dark:hidden"
-                fill
-              />
-              <Image
-                src="./images/about/about-dark-02.svg"
-                alt="About"
-                className="hidden dark:block"
-                fill
-              />
-            </motion.div>
-          </div>
-        </div>
-      </section> */}
-      {/* <!-- ===== About Two End ===== --> */}
     </>
   );
 };
 
-export default About;
+export default HowItWorks;
